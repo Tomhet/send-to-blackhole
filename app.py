2894645141abd48ce8cbf57ff3b9543008fa77f2
Tommaso Alessandrini <12596783+Tomhet@users.noreply.github.com>
2024-05-31 18:15:50 +0200

    feat: support multiple magnet links at once

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from urllib.parse import unquote
 import requests
 import re
 
-regex = r'(?<=btih:)(?P<hash>[^&]*).*&dn=(?P<name>[^&]*)'
+regex = r'urn:btih:(?P<hash>[a-fA-F0-9]{40})(?:&.*dn=(?P<name>[^&]+))?'
 regex_btdig = r'(?<=<title>)(?P<name>[^\s]*)(?= torrent)'
 base_url = 'https://btdig.com/search?q='
 
@@ -13,35 +13,45 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        magnet = request.form.get('magnet')
-        filepath = request.form.get('filepath')
+        magnets = request.form.get('magnet')
+        folder = request.form.get('filepath')
         
-        magnet = str(magnet)
-        valid_input = re.match(r'magnet:\?xt=urn:[a-z0-9]+:[a-zA-Z0-9]{32}', magnet)
-        if not valid_input:
-            return render_template('index.html', error_message='Invalid input. Please enter a valid magnet link.')
+        magnet_links = magnets.splitlines()
+        
+        error_messages = []
+        success_messages = []
 
-        matches = re.search(regex, magnet, re.MULTILINE)
+        for magnet in magnet_links:
+            magnet = str(magnet)
+            valid_input = re.match(r'magnet:\?xt=urn:[a-z0-9]+:[a-zA-Z0-9]{32}', magnet)
 
-        # if no dn in magnet link fall back to scraping btdig
-        if not matches.group('name'):
-            print('falling back to btdig')
-            page = requests.get(base_url+magnet)
-            matches = re.search(regex_btdig, page.text, re.MULTILINE)
+            if not valid_input:
+                error_messages.append(f'Invalid input: {magnet}')
+                continue
+            
+            matches = re.search(regex, magnet)
 
-        # if no name in btdig fall back to using hash as name
-        if not matches.group('name'):
-            render_template('index.html', error_message='Could not get torrent name. Using hash.')
-            torrent_name = matches.group('hash')
+            # if no dn in magnet link fall back to scraping btdig
+            if not matches.group('name'):
+                print('falling back to btdig')
+                page = requests.get(base_url + magnet)
+                matches = re.search(regex_btdig, page.text)
 
-        # make torrent name human readable
-        torrent_name = unquote(matches.group('name'))
+            # if no name in btdig fall back to using hash as name
+            if not matches.group('name'):
+                torrent_name = matches.group('hash')
+                error_messages.append(f'Could not get torrent name for magnet: {magnet}. Using hash: {torrent_name}.')
+            else:
+                # make torrent name human readable only if it isnt the hash
+                torrent_name = unquote(matches.group('name'))
 
-        filepath = 'symlinks/' + filepath + torrent_name + '.magnet'
-        with open(filepath, 'w') as file:
-            file.write(magnet)
+            filepath = 'symlinks/' + folder + torrent_name + '.magnet'
+            with open(filepath, 'w') as file:
+                file.write(magnet)
 
-        return render_template('index.html', ok_message=f'Successfully sent {torrent_name} to blackhole!')
+            success_messages.append(f'Successfully sent {torrent_name} to blackhole!')
+
+        return render_template('index.html', success_messages=success_messages, error_messages=error_messages)
 
     return render_template('index.html')
  
